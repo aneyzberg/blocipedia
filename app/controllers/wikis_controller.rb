ce8e69efb7e1148ecff3b162dd6db117c22f27eb
Anna Neyzberg <aneyzberg@gmail.com>
2014-08-15 08:37:48 +0000

    finishin wiki-controller views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,33 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def edit
+    wiki = Wiki.find(params[:id])
   end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.required(:wiki).permit(:title, :bdoy))
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the Wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def create
+    @wiki = Wike.new(params.require(:wiki).permit(:title, :body)
+    if wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect to @wiki
+    else 
+      flash[:error] = "There was an error saving your wiki. Please try again."
+    end
+  end
+
+
 end
